06bee5a69850a2a4325dc7ef24a7052ef7f72206
wb <wblucky@126.com>
2016-07-18 15:49:46 +0800

    完成头部

--- app/NewsHeader.js
+++ app/NewsHeader.js
@@ -5,6 +5,7 @@ import imageLogo from './y18.gif';
 import './NewsHeader.css';
 
 export default class NewsHeader extends React.Component {
+  // 获取logo
   getLogo() {
     return (
         <div className="newsHeader-logo">
@@ -12,18 +13,72 @@ export default class NewsHeader extends React.Component {
         </div>
         );
   }
+  // 获取title
   getTitle() {
       return (
           <div className="newsHeader-title">
             <a className="newsHeader-textLink" href="http://localhost:8080/">Hacker News</a>
           </div>
           );
-    }
+  }
+  // 获取导航条
+  getNav() {
+    var navLinks = [
+      {
+         name: 'new',
+         url: 'newest'
+      },
+      {
+         name: 'comments',
+         url: 'newcomments'
+      },
+      {
+         name: 'show',
+         url: 'show'
+      },
+      {
+         name: 'ask',
+         url: 'ask'
+      },
+      {
+         name: 'jobs',
+         url: 'jobs'
+      },
+      {
+         name: 'submit',
+         url: 'submit'
+      }
+    ];
+
+    return (
+        <div className="newsHeader-nav">
+          {
+            navLinks.map(function(navLink){
+              return (
+                  <a key={navLink.url} className="newsHeader-navLink newsHeader-textLink" href={"http://localhost:8080/" + navLink.url} >
+                    {navLink.name}
+                  </a> 
+                  );
+            })
+          }
+        </div>
+        );
+  }
+  // 登录入口
+  getLogin() {
+    return (
+        <div className="newsHeader-login">
+          <a className="newsHeader-textLink" href="http://localhost:8080/login?goto=news">login</a>
+        </div>
+        );
+  }
   render() {
     return (
         <div className="newsHeader">
           {this.getLogo()}
           {this.getTitle()}
+          {this.getNav()}
+          {this.getLogin()}
         </div>
         );
   }
