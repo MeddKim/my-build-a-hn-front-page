56d141a381f4953438a160dba868acc9ea67ee68
wb <wblucky@126.com>
2016-07-18 16:11:09 +0800

    newitem part2.1

--- app/NewsItem.js
+++ app/NewsItem.js
@@ -2,13 +2,28 @@
 
 import React from 'react';
 import './NewsItem.css';
+import URL from 'url';
 
 export default class NewsItem extends React.Component {
+
+  getDomain() {
+    return URL.parse(this.props.item.url).hostname;
+  }
+  getTitle() {
+    return (
+        <div className="newsItem-title">
+          <a className="newsItem-titleLink" href={this.props.item.url}>{this.props.item.title}</a>
+          <span className="newsItem-domain"><a href={'http://localhost:8080/from?site=' + this.getDomain()}>({this.getDomain()})</a></span>
+        </div>
+        );
+  }
 
   render() {
     return (
         <div className="newsItem">
-          <a className="NewsItem-titleLink" href={this.props.item.url}>{this.props.item.title}</a>
+          <div className="NewsItem-itemText">
+            {this.getTitle()}
+          </div>
         </div>
         );
   }
